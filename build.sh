cf5e0a5ef2c62918ab7c6bc8d38d49a7a35d3a4a
Daniel O. Delgado <danielo.delgado@gmail.com>
2025-08-05 11:10:24 -0600

    deploys static site

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 src/gen_static_site/main.py -b /boot-dev-static-site/
--- main.sh
+++ main.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 python3 src/gen_static_site/main.py
+cd public && python3 -m http.server 8888
